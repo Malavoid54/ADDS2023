0b31fda837b1c634a62e2b7c2f80285e5064ce5c
PurpleLion54 <fergus.weinert@gmail.com>
2023-03-06 22:21:17 +1030

    Inital build

--- Referee.cpp
+++ Referee.cpp
@@ -7,7 +7,7 @@ Referee::Referee() {
 Player* Referee::refGame(Player * player1, Player * player2) {
     char p1move = player1->makeMove();
     char p2move = player2->makeMove();
-    if ((p1move == p2move) || (p2move == p1move)){
+    if ((p1move == p2move)){
         return nullptr; // tie
     }else if ((p1move == 'R' && p2move == 'S') || (p1move == 'S' && p2move == 'P') || (p1move == 'P' && p2move =='R')){
         return player1;
